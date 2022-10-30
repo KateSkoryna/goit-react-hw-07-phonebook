da7a4c696323156a3dfe917a706715732ff6f319
Kateryna Skoryna <k.skoryna@gmail.com>
2022-10-30 17:37:52 +0100

    Changed store

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,41 +4,36 @@ import {
   ContactListBtn,
   ContactListText,
 } from './ContactList.styled';
-import {
-  selectContacts,
-  selectFilterValue,
-  selectLoadingStatus,
-} from 'redux/selectors';
+import { selectContactsByName, selectLoadingStatus } from 'redux/selectors';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContact } from 'redux/operations';
 import Loader from 'components/Loader';
 
 const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
+  const contacts = useSelector(selectContactsByName);
   const isLoading = useSelector(selectLoadingStatus);
-  const { filter } = useSelector(selectFilterValue);
+  // const { filter } = useSelector(selectFilterValue);
 
-  const filterContactsOnChange = () => {
-    if (!filter) {
-      return contacts;
-    }
-    console.log(contacts);
-    console.log(filter);
-    return contacts.filter(contact =>
-      contact.name.toUpperCase().includes(filter.toUpperCase())
-    );
-  };
-
-  const sortContactsByName = () =>
-    [...filterContactsOnChange()].sort((firstContact, secondContact) =>
-      firstContact.name.localeCompare(secondContact.name)
-    );
+  // const sortContactsByName = () => {
+  //   if (!filter) {
+  //     return [...contacts].sort((firstContact, secondContact) =>
+  //       firstContact.name.localeCompare(secondContact.name)
+  //     );
+  //   }
+  //   return [...contacts]
+  //     .sort((firstContact, secondContact) =>
+  //       firstContact.name.localeCompare(secondContact.name)
+  //     )
+  //     .filter(contact =>
+  //       contact.name.toUpperCase().includes(filter.toUpperCase())
+  //     );
+  // };
 
   return (
     <ContactListBox>
       {isLoading && <Loader />}
-      {sortContactsByName().map(({ id, name, phone }) => (
+      {contacts.map(({ id, name, phone }) => (
         <ContactListItem key={id}>
           <ContactListText>Name: {name}</ContactListText>
           <ContactListText>Number: {phone}</ContactListText>
